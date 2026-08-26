90c8614e1a44f162c9c7d8203daab77d89743663
youta <youtamago0527@gmail.com>
2026-08-26 14:53:31 +0900

    Adjust battle portrait framing

--- js/start-flow.js
+++ js/start-flow.js
@@ -28,7 +28,7 @@
       try {
         [this.prologue, this.characters] = await Promise.all([
           fetch('data/prologue.json').then(response => response.json()),
-          fetch('data/characters.json?v=0.4.11').then(response => response.json())
+          fetch('data/characters.json?v=0.4.13').then(response => response.json())
         ]);
       } catch (error) {
         console.error('Start flow data could not be loaded.', error);
